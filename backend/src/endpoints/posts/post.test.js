8d6b8f051a0e59b312c35f11385c9981b52f7bf2
Jose Ramón Bogarin Valenzuela <bogarin617@gmail.com>
2019-11-05 18:14:05 -0800

    corection test

--- backend/src/endpoints/posts/post.test.js
+++ backend/src/endpoints/posts/post.test.js
@@ -1,30 +1,28 @@
-const supertest = require("supertest");
+const request = require("supertest");
 const app = require("../../server");
-const http = require("http");
-let server;
-let request;
 
 describe("server", () => {
   describe("Endpoints", () => {
     describe("post POST", () => {
-      beforeAll(done => {
-        server = http.createServer(app);
-        console.log(server);
-        server.listen(done);
-        request = supertest(server);
+      beforeAll(async done => {
+        server = app.listen(4000, () => {
+          global.agent = request.agent(server);
+          done();
+        });
+      });
+
+      afterAll(async () => {
+        await server.close();
       });
 
       it("should create new post", async () => {
-        const response = await request(app)
+        const response = await request(server)
           .post("/")
           .send({ userId: 5 })
           .set("user_id", "1")
           .set("Content-type", "application/json");
         expect(response.status).toEqual(201);
         expect(response.body.userId).toEqual(5);
-      });
-      afterAll(done => {
-        server.close(done);
       });
     });
   });
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const { posts } = require("./endpoints");
 const { authenticate } = require("./middleware");
 const app = express();
-const server = "dev";
-
+const run = "test";
 //parse application/x-www-from-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 //parse aplication/json
@@ -14,7 +15,9 @@ app.use(bodyParser.json());
 //injeccion de dependencias
 const postsHandlers = posts({ axios });
 app.post("/", authenticate, postsHandlers.post);
-if (server === "test") {
+
+if (run === "dev") {
+  console.log("entro en produccion");
   app.listen(3001, function() {
     console.log("Example app listening on port 3001!");
   });
